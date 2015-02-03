431886d257dc037c19948ae9abb447d0d3d5d529
Miguel Mundi <miguelmundiaragones@gmail.com>
2015-02-03 23:30:15 +0100

    de 115 a 172

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -15,6 +15,18 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if (a==0 && b==0 && c==0)
+  	raise TriangleError.new
+  end
+
+  if (c<=0)
+  	raise TriangleError.new
+  end
+
+  if((a+b <= c) || (a+c <= b) || (b+c <= a))
+    raise TriangleError.new
+  end
+
 end
 
 # Error class used in part 2.  No need to change this code.
