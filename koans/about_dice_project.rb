9866a6a96ec90d5cc815c43dc47804c3a6c831ce
Miguel Mundi <miguelmundiaragones@gmail.com>
2015-02-04 12:52:49 +0100

    de 195 a 257

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -6,6 +6,15 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  attr_reader :values
+  # se cogen los valores con el attr_reader
+  def roll(number)
+    @values = (1..number).to_a.shuffle
+  end
+  #el roll(number) crea una variable de instancia (@values) para convertirla en un array [1...6] (.to_a) y para luego barajarlas (.shuffle)
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
